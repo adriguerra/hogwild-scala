22e9250239137500a39fab07959124c139130df5
Adrian Guerra Alejos <adrian.guerra@epfl.ch>
2019-05-02 17:10:23 +0200

    Add Main.scala

---
+++ src/main/Main.scala
@@ -0,0 +1,26 @@
+import SGD._
+import Settings._
+import Utils._
+
+import scala.util.Random
+
+object Main {
+  def main(args: Array[String]) = {
+
+    // Begin the set up
+    val t1 = System.nanoTime()
+
+    // Load data and split it into train/test
+    val data = load_reuters_data(train_path, topics_path, test_paths, "CCAT", true)
+
+    val shuffled = Random.shuffle(List(data))
+    val (train_set, test_set) = shuffled.splitAt(math.ceil(data.length*train_proportion))
+
+    // Initialize weights, training_losses and array containing cumulated durations of epochs
+    var weights = Vector.fill(D)(0.0)
+    var training_losses = Vector.empty[Double]
+    var validation_losses = Vector.empty[Double]
+    var epoch_durations_cum = Vector.empty[Double]
+
+  }
+}
