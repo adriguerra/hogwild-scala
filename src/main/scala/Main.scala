dac00def7d2a5b44accb357af39c65444ab83409
jbelguiz <julien.belguise@epfl.ch>
2019-05-17 19:07:50 +0200

    timer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,6 +10,13 @@ import java.io.File
 
 
 object Main {
+
+  def aggList (list:List[Double],nb: Int): Unit =
+  {
+    val average = list.foldLeft((0.0, 1))((acc, i) => ((acc._1 + (i - acc._1) / acc._2), acc._2 + 1))._1
+    println("Time for operation " + nb + " : " + average)
+  }
+
   def main(args: Array[String]) = {
 
     // Begin the set up
@@ -41,22 +48,35 @@ object Main {
     // Threads of workers computing the gradients and updating the weights
     val nn = train_set.length
     val t2 = System.nanoTime()
+
+    var Timelist1 = List[Double]()
+    var Timelist2 = List[Double]()
+    var Timelist3 = List[Double]()
+    var Timelist4 = List[Double]()
+    var Timelist5 = List[Double]()
+    var Timelist6 = List[Double]()
+
     val threads =
       for (i <- 1 to workers) yield {
         val thread = new Thread {
           override def run {
             while(!this.isInterrupted) {
+              val timeloop = System.nanoTime()
+
               val sampleIndex = Random.nextInt(nn)
+              Timelist1 = (System.nanoTime - timeloop) / 1e9d :: Timelist1
 
               //weights.synchronized {
-                val weights_indices = train_set(sampleIndex)._2._1.keySet
-                val weights_state = weights_indices.map(index => index -> weights(index)).toMap
-
+              val weights_indices = train_set(sampleIndex)._2._1.keySet
+              Timelist2 = (System.nanoTime - timeloop) / 1e9d :: Timelist2
+              val weights_state = weights_indices.map(index => index -> weights(index)).toMap
+              Timelist3 = (System.nanoTime - timeloop) / 1e9d :: Timelist3
                 val gradients = sgd_subset(train_set(sampleIndex), weights_state, regParam, D)
                 //val du = gradients.filter(x => x._2!=0).size
-
+              Timelist4 = (System.nanoTime - timeloop) / 1e9d :: Timelist4
                 gradients.foreach(g => weights.update(g._1, weights_state(g._1) - alpha * gradients(g._1)))
-             //}
+              Timelist5 = (System.nanoTime - timeloop) / 1e9d :: Timelist5
+              //}
           }}
         }
         thread.start()
@@ -66,7 +86,9 @@ object Main {
     // Thread calculating the validation loss
     val thread_val_loss = new Thread{
       override def run{
-        while(validation_loss >= 0.1){
+        while(validation_loss >= 0.18){
+          val timeloop2 = System.nanoTime()
+          Timelist6 = (System.nanoTime - timeloop2) / 1e9d :: Timelist6
           validation_loss = compute_loss(test_set, weights.toMap, regParam) / test_set_length
           println(validation_loss)
         }
@@ -78,6 +100,13 @@ object Main {
 
     val timer_end = (System.nanoTime() - timer_start) / 1e9d
     println("Train duration: " + timer_end)
+
+    aggList (Timelist1,1)
+    aggList (Timelist2,2)
+    aggList (Timelist3,3)
+    aggList (Timelist4,4)
+    aggList (Timelist5,5)
+    aggList (Timelist5,6)
 
     threads.foreach(t => t.interrupt())
     threads.foreach(t => t.join())
--- src/main/scala/Settings.scala
+++ src/main/scala/Settings.scala
@@ -8,11 +8,11 @@ object Settings {
   val batch_size = 1
   val alpha = 0.05f
   val regParam = 1e-5f
-  val topics_path = "./ressources/data/rcv1-v2.topics.qrels"
-  val test_paths = List("/data/datasets/lyrl2004_vectors_test_pt0.dat",
-    "/data/datasets/lyrl2004_vectors_test_pt1.dat",
-    "/data/datasets/lyrl2004_vectors_test_pt2.dat",
-    "/data/datasets/lyrl2004_vectors_test_pt3.dat")
-  val train_path = "./ressources/data/lyrl2004_vectors_train.dat"
+  val topics_path = "/home/julien/data/rcv1-v2.topics.qrels"
+  val test_paths = List("/home/julien/data/datasets/lyrl2004_vectors_test_pt0.dat",
+    "/home/julien/data/datasets/lyrl2004_vectors_test_pt1.dat",
+    "/home/julien/data/datasets/lyrl2004_vectors_test_pt2.dat",
+    "/home/julien/data/datasets/lyrl2004_vectors_test_pt3.dat")
+  val train_path = "/home/julien/data/lyrl2004_vectors_train.dat"
 
 }
